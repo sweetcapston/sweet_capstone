6bf96792ae7bcd80dbf30ee8ecb63ecc1f07ebd6
acrord <acrord12@gmail.com>
2019-05-08 22:34:32 +0900

    socket연결

--- src/components/core/Class.vue
+++ src/components/core/Class.vue
@@ -11,7 +11,34 @@
     </v-content>
   </v-app>
 </template>
+<script>
+/* eslint-disable */
+import Vue from "vue";
+import store from '@/store.js'
+import SocketIO from 'socket.io-client';
+import VueSocketIO from 'vue-socket.io'
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: SocketIO('http://localhost:3000'), //options object is Optional
+    vuex: {
+      store,
+      actionPrefix: "SOCKET_",
+      mutationPrefix: "SOCKET_"
+    }
+  })
+);
+export default {
+  sockets: {
+    connect: function () {
+      console.log('socket connected')
+    }
+  },
+  methods: {
+    
+  }
+}
 
+</script>
 <style lang="scss">
   @import "@/styles/index.scss";
 </style>
--- src/views/QuestionClass.vue
+++ src/views/QuestionClass.vue
@@ -2,5 +2,28 @@
   <v-app>
     <h1> 질문 페이지 </h1>
     <h1>클래스 코드 : {{this.$store.state.currentClass.classCode}}</h1>
+    <sui-button @click="test">Test</sui-button>
   </v-app>
 </template>
+<script>
+/* eslint-disable */
+export default {
+  sockets:{
+    MESSAGE: function(data){
+      console.log("data")
+    }
+  },
+  methods:{
+    test(e){
+      e.preventDefault();
+      this.$socket.emit('chat', {
+        classCode:this.$store.state.currentClass.classCode,
+        userID:this.$store.state.userID,
+        userName:this.$store.state.userName,
+        question:"messsssssage",
+        anonymous:false
+      })
+    },
+  }
+}
+</script>
