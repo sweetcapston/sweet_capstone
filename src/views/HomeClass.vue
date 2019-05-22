41ebca7ff25d8205bef68b0dd5f86990ea647f60
LeeSongA <thddk0317@nate.com>
2019-05-22 20:56:01 +0900

    Add calendar

--- src/views/HomeClass.vue
+++ src/views/HomeClass.vue
@@ -8,7 +8,81 @@
         @click="addClass()"
         v-if="this.$store.getters.getIdentity == 1 && (this.$store.state.checkApply == -1)" >수강하기</v-btn> <!-- 수강하지않고있는 학생에게만 보여지도록.. 학생의 수강리스트에서 -->
     </span>
-  </v-app>
+
+    <v-layout>
+      <v-card
+        class="mx-auto"
+        width="500"
+        hegith="500"
+      > 
+        <v-card-title>
+          <v-flex
+            sm4
+            xs12
+            class="text-sm-left text-xs-center"
+          >
+            <v-btn @click="$refs.calendar.prev()">
+              <v-icon
+                dark
+                left
+              >
+                keyboard_arrow_left
+              </v-icon>
+              Prev
+            </v-btn>
+          </v-flex>
+
+          <v-flex
+            sm4
+            xs12
+            class="text-xs-center"
+          >
+            <v-select
+              v-model="type"
+              :items="typeOptions"
+              label="Type"
+            ></v-select>
+          </v-flex>
+
+          <v-flex
+            sm4
+            xs12
+            class="text-sm-right text-xs-center"
+          >
+            <v-btn @click="$refs.calendar.next()">
+              Next
+              <v-icon
+                right
+                dark
+              >
+                keyboard_arrow_right
+              </v-icon>
+            </v-btn>
+          </v-flex>
+        </v-card-title>
+        
+        <v-card-actions>
+          <v-flex
+            xs12
+            class="mb-3"
+          >
+            <v-sheet 
+              height="400"
+            >
+              <v-calendar
+                ref="calendar"
+                v-model="start"
+                :type="type"
+                :end="end"
+                color="cyan lighten-1"
+              > 
+              </v-calendar>
+            </v-sheet>
+          </v-flex>  
+        </v-card-actions>
+      </v-card>
+    </v-layout>
+  </v-app> 
 </template>
 
 <script>
@@ -17,8 +91,19 @@ import {Stud} from "@/api";
 export default {
   data(){
     return{
+      type: 'month',
+      start: '2019-01-01',
+      end: '2019-01-06',
+      typeOptions: [
+        { text: 'Day', value: 'day' },
+        { text: '4 Day', value: '4day' },
+        { text: 'Week', value: 'week' },
+        { text: 'Month', value: 'month' },
+        { text: 'Custom Daily', value: 'custom-daily' },
+        { text: 'Custom Weekly', value: 'custom-weekly' }
+      ]
     }
-  },
+  }, 
   methods: {
     addClass() {
       Stud.classAdd(this.$store.state.currentClass.classCode).then(res => {
@@ -37,7 +122,7 @@ export default {
     outClass(){
       this.$store.commit("removeCurrentClass");
       this.$router.push({name: 'main'});
-    },
+    }
   }
 };
 </script>
