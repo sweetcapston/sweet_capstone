3a735052f4e61647c191bca3e9f3041d7615ca75
Imseungbae <tmdqo2013@naver.com>
2019-05-23 23:37:17 +0900

    질문클래스 좋아요 기능, 설문 클래스 UI 변경, 통계 클래스 UI 변경

--- src/views/ChartClass.vue
+++ src/views/ChartClass.vue
@@ -1,21 +1,14 @@
 <template>
   <v-container fluid grid-list-md>
     <v-layout row wrap>
-      <v-flex d-flex xs12 sm6 md4>
-        <apexchart width="330" type="donut" :options="options" :series="lists"/>
+      <v-flex d-flex xs12 sm6 md4 style="border:1px solid" align-center justify-center>
+        <apexchart width="330" type="donut" :options="option1" :series="list1"/>
       </v-flex>
-      <v-flex d-flex xs12 sm6 md3>
-        
+      <v-flex d-flex xs12 sm6 md3 style="border:1px solid">
+        <apexchart type="radar" height="350" :options="chartOptions" :series="series"/>
       </v-flex>
-      <v-flex d-flex xs12 sm6 md2 child-flex>
-        <v-card color="green lighten-2" dark>
-          <v-card-text>{{ lorem.slice(0, 90) }}</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex d-flex xs12 sm6 md3>
-        <v-card color="blue lighten-2" dark>
-          <v-card-text>{{ lorem.slice(0, 100) }}</v-card-text>
-        </v-card>
+      <v-flex d-flex xs12 sm6 md5 child-flex style="border:1px solid">
+        <apexchart type="bar" height="350" :options="chartOptions2" :series="series2"/>
       </v-flex>
     </v-layout>
   </v-container>
@@ -23,16 +16,57 @@
 
 <script>
 export default {
-    data: function() {
-      return {
-        lorem: "Lorem ipsum dolor sit amet, mel at cu",
-        options: { labels: ['SWEET', '임승배', '치킨집']},
-        lists:[60,10,20]
-        // series: [{
-        //   name: 'series-1',
-        //   data: [30, 40, 35, 50, 49, 60, 70, 91]
-        // }]
+  data: function() {
+    return {
+      lorem: "Lorem ipsum dolor sit amet, mel at cu",
+      option1: { labels: ["SWEET", "임승배", "치킨집"] },
+      list1: [60, 10, 20],
+      series: [
+        {
+          name: "Series 1",
+          data: [80, 50, 30, 40, 100, 20]
+        }
+      ],
+      chartOptions: {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        title: {
+          text: "Basic Radar Chart"
+        }
+      },
+      series2: [
+        {
+          data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+        }
+      ],
+      chartOptions2: {
+        plotOptions: {
+          bar: {
+            horizontal: true
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: [
+            "South Korea",
+            "Canada",
+            "United Kingdom",
+            "Netherlands",
+            "Italy",
+            "France",
+            "Japan",
+            "United States",
+            "China",
+            "Germany"
+          ]
+        }
       }
-    },
+    };
+    // series: [{
+    //   name: 'series-1',
+    //   data: [30, 40, 35, 50, 49, 60, 70, 91]
+    // }]
+  }
 };
 </script>
