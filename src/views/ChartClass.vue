aac005fe55796c1c5621fc6eea816d839da531ab
Imseungbae <tmdqo2013@naver.com>
2019-06-03 22:24:24 +0900

    퀴즈클래스 UI 수정

--- src/views/ChartClass.vue
+++ src/views/ChartClass.vue
@@ -3,27 +3,27 @@
     <v-flex xs12 sm12 md6 lg6 xl6 align-center justify-center>
       <!-- TODO: 질문 -->
       <v-flex row md12 lg12 xl12 style=" height:50%; padding:5px 8px 0px 5px">
-        <material-card color="cyan" title="질문 클래스 통계" text="Question Data">
+        <material-card color="crimson" title="질문 클래스 통계" text="Question Data">
           <apexchart type="bar" height="300" :options="chartOptions" :series="series"/>
         </material-card>
       </v-flex>
       <!-- TODO: 퀴즈 -->
       <v-flex row wrap md12 style="height:50%; padding:0px 8px 5px 5px;">
-        <material-card color="cyan" title="퀴즈 클래스 통계" text="Quiz Data">
-          <v-layout  fill-height>
-          <v-flex lg6 style="background:beige;">
-            <h4>퀴즈 리스트</h4>
-          </v-flex>
-          <v-flex lg6>
-            <apexchart width="330" type="donut" :options="option1" :series="list1"/>
-          </v-flex>
+        <material-card color="crimson" title="퀴즈 클래스 통계" text="Quiz Data">
+          <v-layout fill-height>
+            <v-flex lg6 style="background:beige;">
+              <h4>퀴즈 리스트</h4>
+            </v-flex>
+            <v-flex lg6>
+              <apexchart type=bar height=350 :options="chartOptions2" :series="series2" />
+            </v-flex>
           </v-layout>
         </material-card>
       </v-flex>
     </v-flex>
     <!-- TODO: 설문 -->
     <v-flex wrap xs12 sm12 md6 lg6 xl6 child-flex style="padding:5px 5px 5px 8px;">
-      <material-card color="cyan" title="설문 클래스 통계" text="Survey Data">
+      <material-card color="crimson" title="설문 클래스 통계" text="Survey Data">
         <v-expansion-panel v-if="Identity==1">
           <StudentList
             v-for="n in surveyList.length"
@@ -118,6 +118,91 @@ export default {
       surveyList: [],
       completeList: [],
       formShow: false,
+      series2: [
+        {
+          data: [70, 100, 30, 50, 55, 85]
+        }
+      ],
+      chartOptions2: {
+        plotOptions: {
+          bar: {
+            barHeight: "100%",
+            distributed: true,
+            horizontal: true,
+            dataLabels: {
+              position: "bottom"
+            }
+          }
+        },
+        colors: [
+          "#33b2df",
+          "#546E7A",
+          "#d4526e",
+          "#13d8aa",
+          "#A5978B",
+          "#2b908f",
+          "#f9a3a4",
+          "#90ee7e",
+          "#f48024",
+          "#69d2e7"
+        ],
+        dataLabels: {
+          enabled: true,
+          textAnchor: "start",
+          style: {
+            colors: ["#fff"]
+          },
+          formatter: function(val, opt) {
+            return opt.w.globals.labels[opt.dataPointIndex] + ":  " + val;
+          },
+          offsetX: 0,
+          dropShadow: {
+            enabled: true
+          }
+        },
+
+        stroke: {
+          width: 1,
+          colors: ["#fff"]
+        },
+        xaxis: {
+          categories: [
+            "내 점수",
+            "Max",
+            "Min",
+            "Mid",
+            "Avg",
+            "Top5 Avg",
+          ]
+        },
+        yaxis: {
+          labels: {
+            show: false
+          }
+        },
+        title: {
+          text: "n번째 퀴즈 결과",
+          align: "center",
+          floating: true
+        },
+        subtitle: {
+          text: "퀴즈 응시 결과 입니다.",
+          align: "center"
+        },
+        tooltip: {
+          theme: "dark",
+          x: {
+            show: false
+          },
+          y: {
+            title: {
+              formatter: function() {
+                return "";
+              }
+            }
+          }
+        }
+      },
       series: [
         {
           name: "Servings",
